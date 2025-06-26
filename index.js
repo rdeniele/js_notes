d4dda0d62976b130b644ad7811f50e09e975a80f
rdeniele <rondenielep13@gmail.com>
2025-06-27 00:33:49 +0800

    Initial commit - JavaScript learning notes

---
+++ index.js
@@ -0,0 +1,28 @@
+// var petDog = "Rex";
+// var petCat = "Pepper";
+
+// console.log(petDog);
+// console.log(petCat);
+
+// console.log("My pet dog's name is " + petDog);
+// console.log("My pet cat's name is " + petCat);
+
+
+// var catSound = "purr";
+// var dogSound = "woof";
+
+// console.log(petDog,"says", dogSound);
+// console.log(petCat,"says", catSound);
+
+// catSound= "meow";
+// console.log(petCat,"now says", catSound);
+
+
+// important data types in js
+// String
+// Number
+// Boolean
+// Undefined
+// Null
+// BigInt
+// Symbol
